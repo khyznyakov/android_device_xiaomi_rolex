a3d4a966400c9e84e0274b0e59626cdbf71e15a3
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-07-17 09:44:33 -0500

    rolex: disable warnings sh for now

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 # Patch build error thanks @Ze00
-sh device/xiaomi/rolex/kernel/warnings-errors.sh
+# sh device/xiaomi/rolex/kernel/warnings-errors.sh
 
 # lunch
 add_lunch_combo lineage_rolex-userdebug
